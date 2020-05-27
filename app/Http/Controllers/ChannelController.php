c36bafd67bef9ceab097c46c5c43971503b90885
Ahmed Maruf <maruf@cefalo.com>
2020-05-27 18:49:12 +0600

    Indent code

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Channel;
-use App\Thread;
 use Illuminate\Http\Request;
 
 class ChannelController extends Controller
@@ -15,10 +14,9 @@ class ChannelController extends Controller
      */
     public function index(Channel $channel)
     {
-        if ($channel->exists){
+        if ($channel->exists) {
             $threads = $channel->threads()->latest()->get();
-        }
-        else{
+        } else {
             $threads = [];
         }
         return view('threads.index', compact('threads'));
