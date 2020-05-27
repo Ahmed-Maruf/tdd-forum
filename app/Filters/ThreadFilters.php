269c90a36f66245ba07b910b5988cab692f933a1
Ahmed Maruf <maruf@cefalo.com>
2020-05-27 18:52:44 +0600

    Filter the thread via username

---
+++ app/Filters/ThreadFilters.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class ThreadFilters extends Filters
+{
+
+    protected $filters = ['by'];
+
+    /**
+     * @param $username
+     *
+     * @return mixed
+     */
+
+    protected function by($username)
+    {
+        $user = User::where('name', $username)->firstOrFail();
+
+        return $this->builder->where('user_id', $user->id);
+    }
+
+}
